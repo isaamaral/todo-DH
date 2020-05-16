ba9557b6cee045b45defdbd0a5956b34313a0714
isaamaral <isa_marques_@hotmail.com.br>
2020-05-16 12:01:02 -0300

    adicionou a lógica de criação do to do e renderizou os itens na tela

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+let todoItems = [];
+
+
+// função adiciona toDo
+function addTodo(text) {
+    const todo = {
+        text,
+        checked: false,
+        id: Date.now(),
+    }
+    todoItems.push(todo);
+    console.log("LISTA - ", todoItems);
+
+    // referenciando ul no html
+    const list = document.querySelector(".js-todo-list");
+    list.insertAdjacentHTML("beforeend", `
+        <li class="todo-item" data-key="${todo.id}">
+            <span> ${todo.text} </span>
+            <button class="delete-todo js-delete-todo">
+                <svg>
+                    <use href="#delete-icon"> </use>
+                </svg>
+            </button>
+        </li> 
+    `)
+}
+
+
+// verifica evento formulario e valor input
+const form = document.querySelector(".js-form");
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    const input = document.querySelector(".js-todo-input");
+
+    var text = input.value.trim();
+    if(text !== " ") {
+        addTodo(text);
+        input.value = " ";
+        input.focus();
+    }
+})
+
+// form.addEventListener('submit', event =>{}) //
+
+// 
